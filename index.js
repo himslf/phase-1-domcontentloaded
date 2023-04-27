082b7901b84d4de399d04cb45a4185e5ca940c3c
JASON YOUNG <moorslf@gmail.com>
2023-04-27 03:23:41 -0400

    DOM Content Loaded

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log(`Logged AFTER DOM! READY.`);
+    document.getElementById('text').textContent = "This is really cool!";
+});
+
